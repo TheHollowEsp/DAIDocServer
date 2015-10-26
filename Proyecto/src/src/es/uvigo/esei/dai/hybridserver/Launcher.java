9484c296fde27cd4c116d5366d062b5b3187835a
TheHollowEsp <hrlopez@esei.uvigo.es>
2015-10-26 16:35:45 +0100

    Actualización de launcher
    Ahora también acepta un objeto Properties por línea de comandos (Run
    as...)

--- Proyecto/src/src/es/uvigo/esei/dai/hybridserver/Launcher.java
+++ Proyecto/src/src/es/uvigo/esei/dai/hybridserver/Launcher.java
@@ -1,18 +1,48 @@
 package es.uvigo.esei.dai.hybridserver;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Properties;
+
+/*
+ * TODO: Hilo de servicio para el ExecutorService
+ * TODO: Generar errores 400, 404, 500
+ * TODO: Poner JDBC a funcionar
+ * TODO: Permitir POST (Crear formulario, manejarlo y meter a JDBC o a mapa)
+ * TODO: Permitir DELETE (Borrar pagina)
+ */
 
 public class Launcher {
 	public static void main(String[] args) {
-		// Basico
-		//HybridServer HY = new HybridServer();
-		// Con mapa de paginas
-		Map<String, String> pages = new HashMap<String,String>();
+		HybridServer HY = null;
+		/* Borrar antes de entrega */
+		Map<String, String> pages = new LinkedHashMap<String, String>();
 		pages.put("1234", "<html><h1> Hello 1234 </h1></html>");
 		pages.put("1235", "<html><h1> Hello 1235 </h1></html>");
-		HybridServer HY = new HybridServer(pages);
-		System.out.println("iniciado");
+		pages.put("1236", "<html><h1> Hello 1236 </h1></html>");
+		pages.put("1237", "<html><h1> Hello 1237 </h1></html>");
+		pages.put("1238", "<html><h1> Hello 1238 </h1></html>");
+		/* END Borrar antes de entrega */
+
+		if (args.length > 0) {	//////////////////////////////////////////////////////////////// Con Properties
+			Properties properties = new Properties();
+			try (BufferedReader br = new BufferedReader(new FileReader("config.props"))) {
+				properties.load(br);
+			} catch (IOException e) {
+				System.err.println("Error");
+			} finally {
+				HY = new HybridServer(properties);
+			}
+
+		} else if (pages != null) {	//////////////////////////////////////////////////////////// Con mapa de paginas
+			HY = new HybridServer(pages);
+		} else { /////////////////////////////////////////////////////////////////////////////// Basico
+			HY = new HybridServer();
+		}
+		System.out.println("HybridServer iniciado");
 		HY.start();
 	}
 }
